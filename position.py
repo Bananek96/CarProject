7caa0accfa1991d888538b852a6e6fd6cf216df7
mateusz.bartczak <mateusz.bartczak@pwr.edu.pl>
2024-11-14 16:12:16 +0100

    add init position on the map

--- position.py
+++ position.py
@@ -3,7 +3,7 @@ class Position:
 	"""
 		angle = 0, pojazd skierowany na północ
 	"""
-	def __init__(self):
-		self.x: float = 0
-		self.y: float = 0
+	def __init__(self, init_x: float, init_y: float):
+		self.x: float = init_x
+		self.y: float = init_y
 		self.angle: float = 0
